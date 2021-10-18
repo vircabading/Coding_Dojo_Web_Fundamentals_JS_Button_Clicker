a17945b681d6cd57a9a0f5158f5344c5bee5ce75
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-17 21:12:16 -0700

    add add_likes button function

--- script.js
+++ script.js
@@ -5,3 +5,13 @@
 // Created:     October 16, 2021
 //////////////////////////////////////////////////
 
+//// FUNCTION DECLERATION SECTION ////////////////
+
+function add_likes(element) {                               // Function adds 1 to likes and gives alert that ninja was liked
+    var str = element.innerText;                            // get what is written inside of element
+    var match = str.match(/(\d+)/);                         // get the number in the string
+    var temp_num = match[0];
+    temp_num++;                                             // add 1 to the number of likes in the string
+    element.innerText = temp_num + " likes";                // update the number of likes
+    alert("Ninja was liked");                               // give out alert text box saying ninja was liked
+}
